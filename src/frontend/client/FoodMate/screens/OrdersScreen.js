b00223d8fd4cae633f305f1b663782170818ab6d
LDSPrgrm <ldsprgrm@gmail.com>
2023-12-12 18:01:39 +0800

    modified:   src/frontend/client/FoodMate/screens/OrdersScreen.js

--- src/frontend/client/FoodMate/screens/OrdersScreen.js
+++ src/frontend/client/FoodMate/screens/OrdersScreen.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 
 const OrdersScreen = () => {
   return (
-    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-      <Text>Orders Screen</Text>
+    <View style={{flex:1}}>
+      <View>
+        <Text 
+          style={{
+            alignSelf: 'center',
+            fontSize: 18,
+            fontWeight: 700,
+            marginTop: 10,
+
+          }}>Past Transactions</Text>
+      </View>
+
+      <ScrollView style={{
+        margin: 20
+      }}>
+        <View style={{marginVertical: 10}}>
+          <Text>Product:</Text>
+        </View>
+      </ScrollView>
+
     </View>
   )
 }
